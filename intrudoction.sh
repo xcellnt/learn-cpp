a9e0f3120bfe47cf7d1da48aee3105dc7c9141df
xcellnt <goatbooster@outlook.com>
2022-04-16 06:35:50 +0700

    first commit

---
+++ intrudoction.sh
@@ -0,0 +1,28 @@
+why cpp
+ - systems programming
+ - Games
+ - Critical infrastructure
+
+who is this course for?
+ - complete beginnner
+
+What you can export from the course
+ - The bare fundamentals
+ - Procedural programming
+ - Lots of handsome code writing
+ - advanced OPP with inherintance and polymorphism
+ - Using the tools : compilers and editors
+ = VS Code with dark theme!
+
+What's expected from U
+ - TIME set aside for the course
+ - Strong drive and willingness to learn
+
+Source code
+ -= Github
+
+Support
+ - Discord
+
+
+20.04 
